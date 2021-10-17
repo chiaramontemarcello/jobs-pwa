3a154461a9f74c242fc160480543f3289ee357ee
Marcello Chiaramonte <marcello@chiaramonte.me>
2021-10-17 19:42:13 +0200

    Clean up, remove old example

--- frontend-pwa/source/src/app/app-routing.module.ts
+++ frontend-pwa/source/src/app/app-routing.module.ts
@@ -2,36 +2,20 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
-  },
-  {
-    path: 'message/:id',
-    loadChildren: () =>
-      import('./view-message/view-message.module').then(
-        (m) => m.ViewMessagePageModule
-      ),
-  },
-  {
-    path: '',
-    redirectTo: 'job-list',
-    pathMatch: 'full',
-  },
-  {
-    path: 'job-list',
-    loadChildren: () =>
-      import('./pages/job-list/job-list.module').then(
-        (m) => m.JobListPageModule
-      ),
-  },
+    {
+        path: '',
+        redirectTo: 'job-list',
+        pathMatch: 'full',
+    },
+    {
+        path: 'job-list',
+        loadChildren: () =>
+            import('./pages/job-list/job-list.module').then((m) => m.JobListPageModule),
+    },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-  ],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
